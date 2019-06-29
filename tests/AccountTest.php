778d575ade9d964d01a20f99d116d9c24ed733fd
Oladapo Omonayajo <o.omonayajo@gmail.com>
2019-06-30 01:14:45 +0800

    Added assertion for Account Entity getters

--- tests/AccountTest.php
+++ tests/AccountTest.php
@@ -21,7 +21,7 @@ class AccountTest extends ClientTestCase
     public function testAccountRetrieval(): void
     {
         $account = [
-            'id' => '904837e3-3b76-47ec-b432-046db621571b',
+            'id' => 'id',
             'status' => 'ACTIVE',
             'currency' => 'USD',
             'buying_power' => '0.0',
@@ -54,5 +54,28 @@ class AccountTest extends ClientTestCase
 
         $apiAccount = $this->alpacaClient->getAccount();
         $this->assertEquals($account, $apiAccount->jsonSerialize());
+
+        $this->assertEquals($account['id'], $apiAccount->getId());
+        $this->assertEquals($account['status'], $apiAccount->getStatus());
+        $this->assertEquals($account['currency'], $apiAccount->getCurrency());
+        $this->assertEquals($account['buying_power'], $apiAccount->getBuyingPower());
+        $this->assertEquals($account['cash'], $apiAccount->getCash());
+        $this->assertEquals($account['portfolio_value'], $apiAccount->getPortfolioValue());
+        $this->assertEquals($account['pattern_day_trader'], $apiAccount->isPatternDayTrader());
+        $this->assertEquals($account['trade_suspended_by_user'], $apiAccount->isTradeSuspendedByUser());
+        $this->assertEquals($account['trading_blocked'], $apiAccount->isTradingBlocked());
+        $this->assertEquals($account['transfers_blocked'], $apiAccount->isTransfersBlocked());
+        $this->assertEquals($account['account_blocked'], $apiAccount->isAccountBlocked());
+        $this->assertEquals($account['created_at'], $apiAccount->getCreatedAt());
+        $this->assertEquals($account['shorting_enabled'], $apiAccount->isShortingEnabled());
+        $this->assertEquals($account['multiplier'], $apiAccount->getMultiplier());
+        $this->assertEquals($account['long_market_value'], $apiAccount->getLongMarketValue());
+        $this->assertEquals($account['short_market_value'], $apiAccount->getShortMarketValue());
+        $this->assertEquals($account['equity'], $apiAccount->getEquity());
+        $this->assertEquals($account['last_equity'], $apiAccount->getLastEquity());
+        $this->assertEquals($account['initial_margin'], $apiAccount->getInitialMargin());
+        $this->assertEquals($account['maintenance_margin'], $apiAccount->getMaintenanceMargin());
+        $this->assertEquals($account['daytrade_count'], $apiAccount->getDaytradeCount());
+        $this->assertEquals($account['sma'], $apiAccount->getSma());
     }
 }
