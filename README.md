4d0d61fd1778fa98414cb47f725ea2708a4895ae
Oladapo Omonayajo <o.omonayajo@gmail.com>
2019-07-01 00:16:37 +0800

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Alpaca-PHP
-Alpaca Trade API for PHP
+Alpaca Trade API for PHP [![Build Status](https://travis-ci.org/ragingprodigy/Alpaca-PHP.svg?branch=master)](https://travis-ci.org/ragingprodigy/Alpaca-PHP)
+
+## Overview
+This is a PHP Client for <a href="https://alpaca.markets/">Alpaca</a> (<a href="https://docs.alpaca.markets/api-documentation/web-api/">General Alpaca API Documentation</a>).  Alpaca API lets you build and trade with real-time market data for free.
+
+### Table of Contents
+1. [Installation](#installation)
+2. [Basic Usage](#basic-usage)
+3. [Full Scale Example](#full-scale-example)
+4. [Alpaca API](#alpaca-api)
+5. [Simple Polygon Example](#simple-polygon-example)
+6. [Polygon API](#polygon-api)
+
+#### Installation
+
+The package can be installed via Composer.
+
+    composer require ragingprodigy/alpaca-php
+
+#### Basic Usage
+
+To use the Library, simply create an instance of the client as shown below:
+```php
+use RagingProdigy\Alpaca\Client;
+use RagingProdigy\Alpaca\Config;
+
+// Paper Trading Client
+$client = new Client(new Config('apiKey', 'secretKey'));
+
+// Live Trading Client
+$client = new Client(new Config('apiKey', 'secretKey', false));
+
+// Overriding Trading and Data Api URLs
+$client = new Client(new Config('apiKey', 'secretKey', true, 'https://trading.url/v2/', 'https://data.url/v1/'));
+```
+    
+Under the Hood, the library makes use of the Guzzle Http client library to send HTTP requests. You can also provide a custom ClientInterface as the second argument to the `Client` constructor in order to further customize the GuzzleHttp Client.
+    
+You can then access the available APIs by calling:
+```php    
+$account = $client->getAccount(); // Returns an instance of "Ragingprodigy\Alpaca\Entities\Account"
+$orders = $client->getOrders(); // Returns an array of "Ragingprodigy\Alpaca\Entities\Order" instances
+```
+
+#### Full Scale Example
+
+Here's how you can use the library to print out account information, submit a limit order, and print out bars.
+
+```php
+use Ragingprodigy\Alpaca\Client;
+use Ragingprodigy\Alpaca\Config;
+
+$alpacaClient = new Client(new Config('api.key', 'secret.key'));
+
+$account = $alpacaAccount
+
+```
--- examples/header.php
+++ examples/header.php
@@ -7,17 +7,88 @@ declare(strict_types=1);
  * @license Apache-2.0
  */
 
-use Dotenv\Dotenv;
-
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotEnv = Dotenv::create(__DIR__ . '/../');
-$dotEnv->load();
+use RagingProdigy\Alpaca\Client;
+use RagingProdigy\Alpaca\Config;
+use RagingProdigy\Alpaca\Constants\BarTimeFrame;
+use RagingProdigy\Alpaca\Constants\OrderAction;
+use RagingProdigy\Alpaca\Constants\OrderType;
+use RagingProdigy\Alpaca\Constants\TimeInForce;
 
-$client = new \RagingProdigy\Alpaca\Client(
-    new \RagingProdigy\Alpaca\Config(
-        'ALPACA_API_KEY',
-        'ALPACA_SECRET_KEY',
-        true
-    )
+$alpacaClient = new Client(new Config('PKWVTQ6CRN9286WQTAKY', 'j4Z1SmY0OzMgqnMxW2VQTWn4LA/REsqIzKfD5TJv'));
+
+$account = $alpacaClient->getAccount();
+
+// Get Account Info
+echo "\nAccount Information: \n";
+echo "\tCreated At: {$account->getCreatedAt()}\n";
+echo "\tBuying Power: $ {$account->getBuyingPower()}\n";
+echo "\tPortfolio Value: $ {$account->getPortfolioValue()}\n\n";
+
+// Get Market Clock
+$clock = $alpacaClient->getClock();
+
+echo "Clock: \n";
+echo "\tCurrent Time: {$clock->getTimestamp()}\n";
+echo "\tMarket Next Open Time: {$clock->getNextOpen()}\n";
+echo "\tMarket Next Close Time: {$clock->getNextClose()}\n\n";
+
+// Request Purchase Order
+$order = $alpacaClient->requestNewOrder(
+    'AAPL',
+    5,
+    OrderAction::BUY,
+    OrderType::MARKET,
+    TimeInForce::DAY
 );
+
+echo "Market Order Response: \n";
+echo "\tSymbol: {$order->getSymbol()}\n";
+echo "\tClient Order Id: {$order->getClientOrderId()}\n";
+echo "\tQuantity: {$order->getQuantity()}\n";
+echo "\tType: {$order->getType()}\n";
+echo "\tCreated At: {$order->getCreatedAt()}\n\n";
+
+// TODO: ORDER Update Stream
+
+// Find Order By ID
+$marketOrder = $alpacaClient->getOrder($order->getId());
+
+echo "Market Order by ID Response: \n";
+echo "\tSymbol: {$marketOrder->getSymbol()}\n";
+echo "\tClient Order Id: {$marketOrder->getClientOrderId()}\n";
+echo "\tQuantity: {$marketOrder->getQuantity()}\n";
+echo "\tType: {$marketOrder->getType()}\n";
+echo "\tCreated At: {$marketOrder->getCreatedAt()}\n\n";
+
+// Find Order By Client Order ID
+$orderByClientOrderId = $alpacaClient->getOrderByClientOrderId($order->getClientOrderId());
+
+echo "Market Order by ID Response: \n";
+echo "\tSymbol: {$orderByClientOrderId->getSymbol()}\n";
+echo "\tClient Order Id: {$orderByClientOrderId->getClientOrderId()}\n";
+echo "\tQuantity: {$orderByClientOrderId->getQuantity()}\n";
+echo "\tType: {$orderByClientOrderId->getType()}\n";
+echo "\tCreated At: {$orderByClientOrderId->getCreatedAt()}\n\n";
+
+// Cancel Order
+$alpacaClient->cancelOrder($order->getId()); // Would raise an exception if not successful
+
+// TODO: Order Update STREAM
+
+// Get Bars
+$bars = $alpacaClient->getBars(['AMZN'], BarTimeFrame::DAY, 4);
+
+echo "Bars Response: \n";
+foreach ($bars as $bar) {
+    echo "\t========================\n";
+    echo "\tSymbol: {$bar->getSymbol()}\n";
+    echo sprintf("\tTime: %s\n", (new DateTime($bar->getT()))->format(DATE_ATOM));
+    echo "\tOpen: {$bar->getO()}\n";
+    echo "\tHigh: {$bar->getH()}\n";
+    echo "\tLow: {$bar->getL()}\n";
+    echo "\tClose: {$bar->getC()}\n";
+    echo "\tVolume: {$bar->getV()}\n";
+}
+
